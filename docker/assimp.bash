16458366409ce44e3e05c79c14042880a6a76a1f
mikedh <mik3dh@gmail.com>
2019-02-10 21:45:08 -0500

    switch to master

--- docker/assimp.bash
+++ docker/assimp.bash
@@ -5,13 +5,14 @@ rm -rf assimp
 rm -rf assimp.zip
 
 # download release of assimp
-curl -L -o assimp.zip https://github.com/assimp/assimp/archive/v4.1.0.zip
+# curl -L -o assimp.zip https://github.com/assimp/assimp/archive/v4.1.4.zip
 # check hash of downloaded file
-echo "407be74f44f488fcf1aac3492d962452ddde89561906e917a208c75e1192bcdc  assimp.zip" | sha256sum --check
+# echo "407be74f44f488fcf1aac3492d962452ddde89561906e917a208c75e1192bcdc  assimp.zip" | sha256sum --check
+curl -L -o assimp.zip https://github.com/assimp/assimp/archive/master.zip
 
 # unzip archive
 unzip -qq assimp.zip
-mv assimp-4.1.0 assimp
+mv assimp-master assimp
 
 # build assimp
 cd assimp

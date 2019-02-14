0a54f8af0473b492f9873825cb143a462e2db6cd
Michael Dawson-Haggerty <mik3dh@gmail.com>
2019-02-14 13:47:04 -0500

    add dummy extension for CI tagging

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
- #!/usr/bin/env python
- # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
-from distutils.core import setup
+import setuptools
+
+try:
+    with open('README.md', 'r') as f:
+        README = f.read()
+except BaseException:
+    README = ''
+
+# since pyassimp has no built components (ctypes)
+# we are going to create a dummy source file that
+# setuptools can build to feel proud of itself.
+# if we don't do this the magical CI pipeline tags
+# images as universal rather than platform specific
+with open('assimp/dummy.cpp', 'w') as f:
+    f.write('int main(){return 0;}')
 
 
-README=''
-
-setup(name='assimp',
-      version='4.1.4',
-      license='ISC',
-      description='Python bindings for the Open Asset Import Library (ASSIMP)',
-      long_description=README,
-      url='https://github.com/mikedh/pyassimp',
-      maintainer='Mike Dawson-Haggerty',
-      maintainer_email='mikedh@kerfed.com',
-      packages=['assimp'],
-      requires=['numpy']
-      )
+setuptools.setup(
+    name='assimp',
+    version='4.1.4',
+    license='ISC',
+    description='Python bindings for the Open Asset Import Library (ASSIMP)',
+    long_description=README,
+    url='https://github.com/mikedh/pyassimp',
+    maintainer='Mike Dawson-Haggerty',
+    maintainer_email='mikedh@kerfed.com',
+    packages=['assimp'],
+    requires=['numpy'],
+    ext_modules=[setuptools.Extension('assimp.dummy',
+                                      ['assimp/dummy.cpp'])])
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -6,5 +6,6 @@ class PyAssimpTest(unittest.TestCase):
     def test_import(self):
         import assimp
 
+
 if __name__ == '__main__':
     unittest.main()
